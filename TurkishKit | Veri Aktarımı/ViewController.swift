32a0dfbd432cb67ae46d83c1df0965c071851304
Berk Turan <berk.turan2@isik.edu.tr>
2019-05-16 21:53:52 +0300

    Initial Commit

---
+++ TurkishKit | Veri Aktarımı/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TurkishKit | Veri Aktarımı
+//
+//  Created by Berk Turan on 5/16/19.
+//  Copyright © 2019 Berk Turan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
