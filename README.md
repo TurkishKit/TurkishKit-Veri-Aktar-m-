1a99150598bcbb8f70bf943ea2535d05d706634c
Berk Turan <berk.turan2@isik.edu.tr>
2019-05-16 23:07:29 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TurkishKit-Veri-Aktar-m-
---
+++ TurkishKit | Veri Aktarımı/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  TurkishKit | Veri Aktarımı
+//
+//  Created by Berk Turan on 5/16/19.
+//  Copyright © 2019 Berk Turan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
